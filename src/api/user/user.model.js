50ddaac675cb18a1426a711f5b6dffe63ba3e5fe
Brian Sunday <brians931@gmail.com>
2021-03-01 19:09:12 +0300

    user schema

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -6,6 +6,46 @@ class User extends Model {
     static get tableName() {
         return tableNames.user;
     }
+
+    static modifiers = {
+        defaultSelects(query) {
+            const { ref } = User;
+            query.select(
+                ref("id"),
+                ref("first_name"),
+                ref("role"),
+                ref("last_name"),
+                ref("email"),
+                ref("phone_number"),
+                ref("active"),
+                ref("image_url"),
+                ref("created_at"),
+                ref("isVerified"),
+                ref("updated_at"),
+                ref("active"),
+                ref("password"),
+                ref("verification_token")
+            );
+        },
+
+        defaultSelectsWithoutPass(query) {
+            const { ref } = User;
+            query.select(
+                ref("id"),
+                ref("first_name"),
+                ref("role"),
+                ref("last_name"),
+                ref("email"),
+                ref("phone_number"),
+                ref("active"),
+                ref("image_url"),
+                ref("created_at"),
+                ref("isVerified"),
+                ref("updated_at"),
+                ref("verification_token")
+            );
+        },
+    };
 }
 
 Model.knex(db);
