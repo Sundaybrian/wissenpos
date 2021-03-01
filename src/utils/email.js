7225b9795a97e90825b46fb4b29f7fcfb1442bc7
Brian Sunday <brians931@gmail.com>
2021-03-01 20:18:34 +0300

    upgrade email util

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,9 +1,23 @@
-const nodemailer = require("nodemailer");
-const config = require("../config.json");
+require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+const apikey = process.env.SENDGRID_API_KEY;
+
+sgMail.setApiKey(apikey);
 
 module.exports = sendEmail;
 
-async function sendEmail({ to, subject, html, from = config["emailFrom"] }) {
-    const transporter = nodemailer.createTransport(config["smtpOptions"]);
-    await transporter.sendMail({ from, to, subject, html });
+async function sendEmail({
+    to,
+    subject,
+    html,
+    from = "noreply.wissensof.com",
+}) {
+    const msg = {
+        to,
+        subject,
+        html,
+        from,
+    };
+
+    await sgMail.send(msg);
 }
