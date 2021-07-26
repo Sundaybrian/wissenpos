692ad012c06bb1e62acf0cc6d969be47c04d265a
Brian Sunday <brians931@gmail.com>
2021-07-26 15:49:34 +0300

    add new scripts for docjker and prettier formatting

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const parse = require("pg-connection-string").parse;
 const pgconfig = parse(process.env.DATABASE_URL);
-pgconfig.ssl = { rejectUnauthorized: false };
+pgconfig.ssl = { rejectUnauthorized: false }; // for heroku
 
 module.exports = {
     test: {
