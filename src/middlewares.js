cd7076bafe5c1fe21624f332d8b512b95698d674
Brian Sunday <brians931@gmail.com>
2021-07-27 14:26:13 +0300

    add custom errors

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,23 +1,136 @@
+const {
+  ValidationError,
+  NotFoundError,
+  DBError,
+  ConstraintViolationError,
+  UniqueViolationError,
+  NotNullViolationError,
+  ForeignKeyViolationError,
+  CheckViolationError,
+  DataError,
+} = require('objection');
+
 function notFound(req, res, next) {
-    res.status(404);
-    const error = new Error(`🔍 - Not Found xx - ${req.originalUrl}`);
-    next(error);
+  res.status(404);
+  const error = new Error(`🔍 - Not Found xx - ${req.originalUrl}`);
+  next(error);
 }
 
 /* eslint-disable no-unused-vars */
 function errorHandler(err, req, res, next) {
-    /* eslint-enable no-unused-vars */
-    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-    res.status(statusCode);
-    // if (process.env.NODE_ENV !== "production") console.log(err.message);
-    console.log(err);
-    res.json({
-        message: err.message || err,
-        stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  /* eslint-enable no-unused-vars */
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  // if (process.env.NODE_ENV !== "production") console.log(err.message);
+
+  if (err instanceof ValidationError) {
+    switch (err.type) {
+      case 'ModelValidation':
+        res.status(400).send({
+          message: err.message,
+          type: err.type,
+          data: err.data,
+        });
+        break;
+      case 'RelationExpression':
+        res.status(400).send({
+          message: err.message,
+          type: 'RelationExpression',
+          data: {},
+        });
+        break;
+      case 'UnallowedRelation':
+        res.status(400).send({
+          message: err.message,
+          type: err.type,
+          data: {},
+        });
+        break;
+      case 'InvalidGraph':
+        res.status(400).send({
+          message: err.message,
+          type: err.type,
+          data: {},
+        });
+        break;
+      default:
+        res.status(400).send({
+          message: err.message,
+          type: 'UnknownValidationError',
+          data: {},
+        });
+        break;
+    }
+  } else if (err instanceof NotFoundError) {
+    res.status(404).send({
+      message: err.message,
+      type: 'NotFound',
+      data: {},
     });
+  } else if (err instanceof UniqueViolationError) {
+    res.status(409).send({
+      message: err.message,
+      type: 'UniqueViolation',
+      data: {
+        columns: err.columns,
+        table: err.table,
+        constraint: err.constraint,
+      },
+    });
+  } else if (err instanceof NotNullViolationError) {
+    res.status(400).send({
+      message: err.message,
+      type: 'NotNullViolation',
+      data: {
+        column: err.column,
+        table: err.table,
+      },
+    });
+  } else if (err instanceof ForeignKeyViolationError) {
+    res.status(409).send({
+      message: err.message,
+      type: 'ForeignKeyViolation',
+      data: {
+        table: err.table,
+        constraint: err.constraint,
+      },
+    });
+  } else if (err instanceof CheckViolationError) {
+    res.status(400).send({
+      message: err.message,
+      type: 'CheckViolation',
+      data: {
+        table: err.table,
+        constraint: err.constraint,
+      },
+    });
+  } else if (err instanceof DataError) {
+    res.status(400).send({
+      message: err.message,
+      type: 'InvalidData',
+      data: {},
+    });
+  } else if (err instanceof DBError) {
+    res.status(500).send({
+      message: err.message,
+      type: 'UnknownDatabaseError',
+      data: {},
+    });
+  } else {
+    res.status(500).send({
+      message: err.message,
+      type: 'UnknownError',
+      data: {},
+    });
+  }
+
+  // res.json({
+  //     message: err.message || err,
+  //     stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  // });
 }
 
 module.exports = {
-    notFound,
-    errorHandler,
+  notFound,
+  errorHandler,
 };
