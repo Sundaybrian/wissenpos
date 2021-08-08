bec8e4a83e43255630dee635d40783291ce97cb5
Brian Sunday <brians931@gmail.com>
2021-08-08 16:34:01 +0300

    update: serverjs to use cluster fork

--- src/server.js
+++ src/server.js
@@ -1,9 +1,31 @@
-﻿const app = require('./app');
+﻿const cluster = require('cluster');
+const os = require('os');
+const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
+// Check if current process is master.
+if (cluster.isMaster) {
+  // Get total CPU cores.
+  const cpuCount = os.cpus().length;
+
+  // Spawn a worker for every core.
+  for (let j = 0; j < cpuCount; j++) {
+    cluster.fork();
+  }
+} else {
+  // This is not the master process, so we spawn the express server.
+  app.listen(port, () => {
+    /* eslint-disable no-console */
+    console.log(`Listening: http://localhost:${port}`);
+    /* eslint-enable no-console */
+  });
+}
+
+// Cluster API has a variety of events.
+// Here we are creating a new process if a worker die.
+cluster.on('exit', function (worker) {
+  console.log(`Worker ${worker.id} died'`);
+  console.log(`Staring a new one...`);
+  cluster.fork();
 });
